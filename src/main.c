e06a4b7de291ae6588fe426499f5a6f65c1c32df
clcwdans <1340397059@qq.com>
2026-01-22 03:44:00 +0800

    invalid-command test4

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
@@ -9,6 +10,7 @@ int main(int argc, char *argv[]) {
   printf("$ ");
   char buff[256];
   fgets(buff, sizeof(buff), stdin);
+  buff[strlen(buff)-1] = '\0';
   printf("%s: command not found\n", buff);
   return 0;
 }
