b0f68505da8f0c5f622b17a32d03e6b882f729b5
clcwdans <1340397059@qq.com>
2026-01-29 23:12:07 +0800

    cd demo2

--- src/cd.c
+++ src/cd.c
@@ -5,7 +5,7 @@
 char *builtin_cd(const char *args) {
     // chdir 在 windows 下依然是用不了的
     int ret = chdir(args);
-    if (!ret) {
+    if (ret == -1) {
         printf("cd: %s: No such file or directory\n", args);
     }
     return NULL;
