72c7a6bb8b869b951ece31f52cf43bcf6067b1fb
clcwdans-wsl_nb <1340397059@qq.com>
2026-02-05 00:39:30 +0800

    single-quote test1

--- src/echo.c
+++ src/echo.c
@@ -9,28 +9,32 @@
 
 char *builtin_echo(const char *content) {
     int len = strlen(content);
-    int in_word = 0;
+    int in_word = 1;
     int quoted = 0;
     for (int i=0; i<len; i++) {
+        
+        if (!quoted && isblank(content[i])) {
+            putchar(' ');
+            while (isblank(content[i]) && i<len) {
+                i ++;
+            }
+        }
+
         if (!quoted && content[i]=='\'') {
             quoted = 1;
+            continue;
         }
         if (quoted && content[i]=='\'') {
             quoted = 0;
-            // printf(" ");
-        }
-        if ()
-        // 占位
-        if (!in_word && isblank(content[i])) {
             continue;
         }
-        if (!in_word && !isblank(content[i])) {
-            printf("%c", )
+
+        if (!quoted || (quoted&&!isblank(content[i]))) {
+            putchar(content[i]);
+            continue;
         }
-        if (in_word) {
-            printf("%c", content[i]);
-        }
+
     }
-    // printf("%s\n", content);
+    putchar('\n');
     return NULL;
 }
