53712a303548b202299192af1cb866036662474b
clcwdans-wsl_nb <1340397059@qq.com>
2026-02-21 23:39:12 +0800

    remove extra files

--- src/make/build.bat
+++ src/make/build.bat
@@ -1,12 +1,11 @@
 @echo off
 if exist "build" (
-    echo removing build
-    rd /s /q "build"
-    echo build removed
+    :: echo removing build
+    :: rd /s /q "build"
+    :: echo build removed
 ) else (
-    echo build not found
+    mkdir build
 )
-mkdir build
 cd build
 cmake ..
 make
